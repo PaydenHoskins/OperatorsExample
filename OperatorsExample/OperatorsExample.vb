4e507928d9685798a7d1117ea304e8ca4e9fdb9c
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-16 09:03:39 -0700

    Beep 1 sec, calulate 5+3

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -1,7 +1,15 @@
-﻿Module OperatorsExample
+﻿'Payden Hoskins
+'Spring 2025
+'RCET2265
+'OperatorsExample
+'https://github.com/PaydenHoskins/OperatorsExample.git
+
+Module OperatorsExample
 
     Sub Main()
-
+        Console.Beep(262, 1000)
+        Console.Write("5 + 3 = ") 'print a string
+        Console.WriteLine(5 + 3) 'Print the sum
     End Sub
 
 End Module
