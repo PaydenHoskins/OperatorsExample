e991a3aad097be72539b7b06ef5cad2b56f26f14
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-17 08:19:12 -0700

    added math operators

--- OperatorsExample/OperatorsExample.vb
+++ OperatorsExample/OperatorsExample.vb
@@ -8,8 +8,27 @@ Module OperatorsExample
 
     Sub Main()
         Console.Beep(262, 1000)
+
+        'sum operator
         Console.Write("5 + 3 = ") 'print a string
         Console.WriteLine(5 + 3) 'Print the sum
+        Console.WriteLine()
+
+        'Subtraction operator
+        Console.Write("5 - 3 = ") 'print a string
+        Console.WriteLine(5 - 3) 'Print the sum
+        Console.WriteLine()
+
+        'multipication operator
+        Console.Write("5 * 3 = ") 'print a string
+        Console.WriteLine(5 * 3) 'Print the sum
+        Console.WriteLine()
+
+        'floating point division operator
+        Console.Write("5 / 3 = ") 'print a string
+        Console.WriteLine(5 / 3) 'Print the sum
+        Console.WriteLine()
+
     End Sub
 
 End Module
